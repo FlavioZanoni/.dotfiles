47f4348deecae194ca0ca9d3ced30a2ed5bc3502
FlavioZanoni <flaviozg888@gmail.com>
2024-08-17 20:48:22 -0300

    (neovim) adding git-conflict.nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -118,6 +118,16 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'GitConflictDetected',
+  callback = function()
+    vim.notify('Conflict detected in ' .. vim.fn.expand '<afile>')
+    vim.keymap.set('n', 'cww', function()
+      vim.engage.conflict_buster()
+      vim.create_buffer_local_mappings()
+    end)
+  end,
+})
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
