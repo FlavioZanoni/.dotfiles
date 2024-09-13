46ca2c121fdc225985bfb0a5ace67308b30ccd37
FlavioZanoni <flaviozg888@gmail.com>
2024-09-12 23:33:52 -0300

    (hypr) updating main monitor on monitors.sh

--- hypr/.config/hypr/monitors.sh
+++ hypr/.config/hypr/monitors.sh
@@ -24,10 +24,9 @@ else
   hyprctl keyword monitor "HDMI-A-1,disable"
 fi
 
-# rebind monitor
-hyprctl kayword monitor "DP-1,3440x1440@100,0x0,1"
-
 # reset monitor
+hyprctl keyword monitor "DP-1,3440x1440@100,1x0,1"
+hyprctl keyword monitor "DP-1,3440x1440@100,0x0,1"
 hyprctl keyword monitor "HDMI-A-1,1920x1080@60,0x0,1,transform,1"
 hyprctl keyword monitor "HDMI-A-1,1920x1080@60,-1080x0,1,transform,1"
 
